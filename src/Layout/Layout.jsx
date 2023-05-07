acaf0b8a821c435c8f0b0be3f54d9487e25f5452
jagjeet singh <itsjagjeet1699@gmail.com>
2023-05-07 16:18:49 +0530

    minor improvements

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,18 +2,35 @@ import React, { useEffect } from "react";
 import Header from "./Header";
 import { getUserInfo } from "../Redux/slices/UserSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { CircularProgress } from "@mui/material";
 
 const Layout = ({ children }) => {
   const dispatch = useDispatch();
+  const location = useRouter();
+  const user = useSelector((state) => state.UserSlice?.userInfo);
 
   useEffect(() => {
-    dispatch(getUserInfo());
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(getUserInfo());
+    } else {
+      location.push("/login");
+    }
   }, []);
 
   return (
     <div className="">
-      <Header />
-      <div style={{ height: "Calc(100vh - 100px)" }}>{children}</div>
+      {Object.keys(user).length > 0 ? (
+        <>
+          <Header />
+          <div style={{ height: "Calc(100vh - 100px)" }}>{children}</div>
+        </>
+      ) : (
+        <div className="w-screen h-screen flex items-center justify-center">
+          <CircularProgress sx={{ color: "gray" }} size={60} />
+        </div>
+      )}
     </div>
   );
 };
